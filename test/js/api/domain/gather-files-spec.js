5fe0bb448aedcfa27bdcc285140d6c7a851183ac
zezutom <zezulatomas@gmail.com>
2015-01-18 18:32:44 +0100

    Pull request comments: tests adjusted to realistic use cases

--- test/js/api/domain/gather-files-spec.js
+++ test/js/api/domain/gather-files-spec.js
@@ -19,6 +19,7 @@ describe('api/domain/gather-files', function() {
         return _.map(filenames, function(filename) {return path.join(fakePath, filename);});
     };
     
+    // returns an array of objects, each of which is defined as { basename: <filename>, file: <absolute file path> }
     var _expected = function(filenames) {
         return _.map(filenames, function(filename) {
             return {name: filename, file: path.join(fakePath, filename)};
@@ -37,29 +38,29 @@ describe('api/domain/gather-files', function() {
 
     });    
     
-    it('should only return saved requests by default', function() {
+    it('should only return all saved POST requests, not the responses associated with them', function() {
         assert.deepEqual(
             _test(_getFiles(['request_a', 'response_a', 'request_b', 'response_b'])), 
             _expected(['request_a', 'request_b']));
 
     });    
-    
-    it('shouldn\'t return anything if there are only saved responses available, by default', function() {
+
+    it('shouldn\'t return anything if there are only saved POST responses available', function() {
         assert.deepEqual(_test(['response_a', 'response_b']), []);
     });    
+        
+    it('should return all saved GET requests', function() {
+        assert.deepEqual(
+            _test(_getFiles(['getA?b=c', 'getB?c=d', 'getC?d=e&f=g'])), 
+            _expected(['getA?b=c', 'getB?c=d', 'getC?d=e&f=g']));
 
+    });        
+    
     it('should be possible to override the default response pattern', function() {
         assert.deepEqual(
             _test(_getFiles(['request_a1', 'response_a1', 'request_a2', 'response_a2', 'request_b', 'response_b']),
                   {patterns: {response: 'response_a'}}), 
             _expected(['request_a1', 'request_a2', 'request_b', 'response_b']));
-    });            
-
-    it('should be possible to flip the response pattern to filter out requests instead of responses', function() {
-        assert.deepEqual(
-            _test(_getFiles(['request_a', 'response_a', 'request_b', 'response_b', 'request_c', 'response_c']),
-                  {patterns: {response: 'request_'}}), 
-            _expected(['response_a', 'response_b', 'response_c']));
     });            
     
 });
