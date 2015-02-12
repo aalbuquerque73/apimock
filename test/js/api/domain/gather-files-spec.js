174695b19cd8db43c58bad20ea2d134ceb775e19
zezutom <zezulatomas@gmail.com>
2015-02-12 23:02:17 +0100

    Revamped tests of GET and POST request search.

--- test/js/api/domain/gather-files-spec.js
+++ test/js/api/domain/gather-files-spec.js
@@ -13,7 +13,7 @@ describe('api/domain/gather-files', function() {
         if(stub) stub.restore();   
     });
     
-    var fakePath = '/fake/path';
+    var fakePath = '/Users/fake/apimock/data';
     
     var _getFiles = function(filenames) {
         return _.map(filenames, function(filename) {return path.join(fakePath, filename);});
@@ -30,37 +30,43 @@ describe('api/domain/gather-files', function() {
         stub = sinon.stub(glob, "sync", function() {return files.sort();});
         return gatherFiles(path.normalize(fakePath), options || {});    
     };
-
-    it('should recognize responses as files starting with \'response_\' by default', function() {
-        assert.deepEqual(
-            _test(_getFiles(['request_a', 'response_a', 'request_b', 'res_b'])), 
-            _expected(['request_a', 'request_b', 'res_b']));
-
-    });    
     
-    it('should only return all saved POST requests, not the responses associated with them', function() {
-        assert.deepEqual(
-            _test(_getFiles(['request_a', 'response_a', 'request_b', 'response_b'])), 
-            _expected(['request_a', 'request_b']));
-
-    });    
-
-    it('shouldn\'t return anything if there are only saved POST responses available', function() {
-        assert.deepEqual(_test(['response_a', 'response_b']), []);
-    });    
-        
     it('should return all saved GET requests', function() {
+        // courtesy MediaWiki API (http://en.wikipedia.org/w/api.php)
+        // The queries below work best when appended to http://en.wikipedia.org/w/api.php?
+        var ghost_busters = 'action=query&format=json&continue=&titles=ghost%20busters.json',
+            et_calls_home = 'action=query&format=json&continue=&titles=E.T.%20the%20Extra-Terrestrial.xml',
+            wild_wild_west = 'action=query&format=xml&continue=&titles=The%20Good,%20the%20Bad%20and%20the%20Ugly.xml';
+                
+        assert.deepEqual(            
+            _test(_getFiles([ghost_busters, et_calls_home, wild_wild_west])), 
+            // files are sorted in alphabetical order
+            _expected([et_calls_home, ghost_busters, wild_wild_west]));
+    
+    });
+    
+    it('should return all saved POST requests and ignore their response counterparts', function() {
+        var post_0 = ['request_0.xml', 'response_0.xml'],
+            post_1 = ['request_1.xml', 'response_1.xml'],
+            post_2 = ['request_2.xml', 'response_2.xml'];
+        
         assert.deepEqual(
-            _test(_getFiles(['getA?b=c', 'getB?c=d', 'getC?d=e&f=g'])), 
-            _expected(['getA?b=c', 'getB?c=d', 'getC?d=e&f=g']));
-
-    });        
+            // all requests and responses
+            _test(_getFiles([post_0[0], post_0[1], post_1[0], post_1[1], post_2[0], post_2[1]])), 
+            // only requests are expected to be returned                                                                    
+            _expected([post_0[0], post_1[0], post_2[0]]));    
+    });
     
     it('should be possible to override the default response pattern', function() {
+        var post_0 = ['req_0.xml', 'res_0.xml'],
+            post_1 = ['req_1.xml', 'res_1.xml'],
+            post_2 = ['req_2.xml', 'res_2.xml'];    
+        
         assert.deepEqual(
-            _test(_getFiles(['request_a1', 'response_a1', 'request_a2', 'response_a2', 'request_b', 'response_b']),
-                  {patterns: {response: 'response_a'}}), 
-            _expected(['request_a1', 'request_a2', 'request_b', 'response_b']));
-    });            
-    
+            // all requests and responses - please note the shortened file names
+            _test(_getFiles([post_0[0], post_0[1], post_1[0], post_1[1], post_2[0], post_2[1]]),
+                  {patterns: {response: 'res_'}}), 
+            _expected([post_0[0], post_1[0], post_2[0]]));
+        
+    });    
 });
