a8e2a3bd1da7a1955efb219876bc5f2f92ce4c99
Americo Albuquerque <americo.albuquerque@mobenga.com>
2015-03-31 13:55:11 +0200

    Fix crash with POST data when override.folder is not an array

--- api/overriders.js
+++ api/overriders.js
@@ -4,6 +4,13 @@ var _ = require('underscore'),
     path = require('path'),
     folders = require('./folders');
 
+function joinIfArray(folder) {
+    if (Array.isArray(folder)) {
+        return folder.join(',');
+    }
+    return folder;
+}
+
 function Overrides() {
     this.save = {
         folders: function(proxy, route) {
@@ -24,49 +31,51 @@ function Overrides() {
                     }, this);
                 } else {
                     var override = route.overrides[proxy.override];
-                    var name = override.name || override.folder.join(',') || 'override';
-                    if (!override.name) {
-                        override.name = path.join(proxy.name, name);
-                    }
-                    if (!folders[override.name]) {
-                        folders[override.name] = folders[proxy.name];
-                        if (override && override.folder) {
-                            if (Array.isArray(override.folder)) {
-                                folders[override.name] = [];
-                                folders[override.name].name = override.name;
-                                folders[override.name].index = 0;
-                                folders[override.name].time = new Date().getTime();
-                                folders[override.name].next = function() {
-                                    ++this.index;
-                                    if (this.index >= this.length) {
-                                        this.index = 0;
-                                    }
-                                }.bind(folders[override.name]);
-                                if (override.type === 'time' && override.delay > 0) {
+                    if (override) {
+                        var name = override.name || joinIfArray(override.folder) || 'override';
+                        if (!override.name) {
+                            override.name = path.join(proxy.name, name);
+                        }
+                        if (!folders[override.name]) {
+                            folders[override.name] = folders[proxy.name];
+                            if (override.folder) {
+                                if (Array.isArray(override.folder)) {
+                                    folders[override.name] = [];
+                                    folders[override.name].name = override.name;
+                                    folders[override.name].index = 0;
+                                    folders[override.name].time = new Date().getTime();
                                     folders[override.name].next = function() {
-                                        if (new Date().getTime() - this.time > override.delay) {
-                                            this.time = new Date().getTime();
-                                            ++this.index;
-                                            if (this.index >= this.length) {
-                                                this.index = 0;
-                                            }
+                                        ++this.index;
+                                        if (this.index >= this.length) {
+                                            this.index = 0;
                                         }
                                     }.bind(folders[override.name]);
-                                }
-                                folders[override.name].current = function() {
-                                    return this[this.index];
-                                }.bind(folders[override.name]);
-                                _.each(override.folder, function(folder) {
-                                    var oFolder = path.join(folders[route.name], folder);
-                                    folders[override.name].push(oFolder);
-                                    if (!fs.existsSync(oFolder)) {
-                                        mkdirp.sync(oFolder);
+                                    if (override.type === 'time' && override.delay > 0) {
+                                        folders[override.name].next = function() {
+                                            if (new Date().getTime() - this.time > override.delay) {
+                                                this.time = new Date().getTime();
+                                                ++this.index;
+                                                if (this.index >= this.length) {
+                                                    this.index = 0;
+                                                }
+                                            }
+                                        }.bind(folders[override.name]);
                                     }
-                                }, this);
-                            } else {
-                                folders[override.name] = path.join(folders[route.name], override.folder);
-                                if (!fs.existsSync(folders[override.name])) {
-                                    mkdirp.sync(folders[override.name]);
+                                    folders[override.name].current = function() {
+                                        return this[this.index];
+                                    }.bind(folders[override.name]);
+                                    _.each(override.folder, function(folder) {
+                                        var oFolder = path.join(folders[route.name], folder);
+                                        folders[override.name].push(oFolder);
+                                        if (!fs.existsSync(oFolder)) {
+                                            mkdirp.sync(oFolder);
+                                        }
+                                    }, this);
+                                } else {
+                                    folders[override.name] = path.join(folders[route.name], override.folder);
+                                    if (!fs.existsSync(folders[override.name])) {
+                                        mkdirp.sync(folders[override.name]);
+                                    }
                                 }
                             }
                         }
