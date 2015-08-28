9d0f5815b667250d1c15b80bb4c9ad7f7c6be75d
Americo Albuquerque <americo.albuquerque@mobenga.com>
2015-08-28 12:07:34 +0200

    Refactor rename to handle {{*}}

--- data/rename.js
+++ data/rename.js
@@ -1,70 +1,86 @@
-var _ = require('underscore'),
+var debug = require('debug'),
+    _ = require('underscore'),
     fs = require('fs'),
     minimist = require('minimist'),
     Q = require('q'),
     path = require('path'),
     glob = require('glob');
 
+var log = debug('rename/logs'),
+    info = debug('rename/info'),
+    warn = debug('rename/warnnings'),
+    error = debug('rename/errors');
+
 var options = {
-    string: [ 'm', 'f', 'p' ],
+    string: [ 'm', 'f', 'p', 's' ],
     boolean: [ 'v' ],
     default: {
-        p: ''
+        f: '',
+        m: '{{*}}',
+        p: '',
+        s: '_'
     },
     alias: {
         v: 'verbose',
         m: 'map',
         f: 'filter',
-        p: 'path'
+        p: 'path',
+        s: 'separator'
     }
 };
 var argv = minimist(process.argv.slice(2), options);
 
-console.log(argv);
-console.log(path.join(argv.path, '*.req'));
-
 var list = glob.sync(path.join(argv.path, '*.req'));
-console.log(list);
+info('Proccessing ' + list.length + ' files');
 
 var promiseList = _.chain(list)
     .map(function(item) {
         return Q.Promise(function(resolve, reject) {
-            console.log('checking', item, '...');
+            log('checking file', item);
             fs.readFile(path.join(__dirname, item), { encoding: 'utf8' }, function(err, text) {
                 if (err) {
                     reject(err);
                     return;
                 }
                 var data = JSON.parse(text);
-                var line = argv.m;
-                var have = [ 0, 0 ], expected = [ 0, 0 ];
-                line = line.replace(/{{(\w+)}}/g, function(match, repl) {
-                    ++expected[0];
-                    if (data.hasOwnProperty(repl)) {
-                        ++have[0];
-                        return data[repl];
-                    }
-                    return match;
-                });
-                if (argv.filter) {
-                    var filter = argv.filter.split(',').map(function(item) { return item.trim(); });
-                    _.each(filter, function(item) {
-                        ++expected[1];
-                        if (data.hasOwnProperty(item)) {
-                            ++have[1];
+                var filter = argv.f.split(',');
+                var test = _.all(filter, function(filter) {
+                    var tokens = filter.split('=');
+                    log(filter, tokens);
+                    var vars = tokens[0].split('|');
+                    return _.all(vars, function(key) {
+                        if (data.hasOwnProperty(key)) {
+                            return data[key].match(tokens[1]);
                         }
-                        var tokens = item.split('=');
-                        if (tokens.length > 1) {
-                            if (data.hasOwnProperty(tokens[0]) && data[tokens[0]] === tokens[1]) {
-                                ++have[1];
+                        return true;
+                    });
+                });
+                if (test) {
+                    var first = true;
+                    var line = argv.m.replace(/{{([\w\d|]+)}}/g, function(match, variable) {
+                        var tokens = variable.split('|');
+                        var sep = first ? '' : argv.s;
+                        first = false;
+                        log(match, variable, tokens);
+                        for (var i = 0; i < tokens.length; ++i) {
+                            if (data.hasOwnProperty(tokens[i])) {
+                                var param = data[tokens[i]];
+                                delete data[tokens[i]];
+                                log(data);
+                                return sep + param;
                             }
                         }
+                        return '';
+                    }).replace('{{*}}', function() {
+                        var sep = first ? '' : argv.s;
+                        return sep + Object.keys(data).map(function(key) { return data[key]; }).join(argv.s);
                     });
+                    log('testing', data, line, filter, test);
+                    resolve({ old: item, 'new': path.join(path.dirname(item), line) });
+                } else {
+                    log('testing', data, filter, test);
+                    reject('filter out ' + item);
                 }
-                if (expected[0] === have[0] && expected[1] === have[1]) {
-                    resolve({ old: item, new: path.join(argv.path, line) });
-                }
-                reject({ match: have[0]===expected[0], filter: have[1]===expected[1], file: item });
             });
         });
     })
@@ -80,15 +96,18 @@ Q.allSettled(promiseList)
                 _.each(item, function(name, key) {
                     files[key] = path.join(__dirname, name);
                 });
-                console.log(files);
-                _.each(['req', 'res', 'stats'], function(ext) {
-                    fs.rename(files.old.replace(/req$/, ext), files.new + '.' + ext, function(err) {
-                        if (err) {
-                            console.error('Error renaming', item.old);
-                            console.error(err);
-                        }
-                    }); 
-                });
+                if (files.old !== (files.new + '.req')) {
+                    _.each(['req', 'res', 'stats'], function(ext) {
+                        fs.rename(files.old.replace(/req$/, ext), files.new + '.' + ext, function(err) {
+                            if (err) {
+                                error('Error renaming', item.old);
+                                error(err);
+                                return;
+                            }
+                            info('file renamed', files);
+                        }); 
+                    });
+                }
             });
         
         //var err = _.chain(list)
